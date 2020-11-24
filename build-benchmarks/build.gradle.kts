6e8053081c166eb5deb2ac6128daf32ec288170f
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2020-11-24 15:27:23 +0300

    Use specified version of Kotlin to compile benchmark scenarios

--- build-benchmarks/build.gradle.kts
+++ build-benchmarks/build.gradle.kts
@@ -1,15 +1,36 @@
 plugins {
-    kotlin("jvm") version "1.4.0"
     application
 }
 
 group = "org.jetbrains"
 version = "unspecified"
 
+buildscript {
+    val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.4.255-SNAPSHOT"
+    val kotlinRepo = "https://buildserver.labs.intellij.net/guestAuth/app/rest/builds/buildType:(id:Kotlin_KotlinDev_CompilerDistAndMavenArtifacts),number:$kotlinVersion,branch:default:any/artifacts/content/maven"
+    extra["kotlinRepo"] = kotlinRepo
+
+    repositories {
+        mavenLocal()
+        maven {
+            url = uri(kotlinRepo)
+        }
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+apply(plugin = "kotlin")
+
+val kotlinRepo: String by extra
 repositories {
-    jcenter()
     maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
     mavenLocal()
+    maven {
+        url = uri(kotlinRepo)
+    }
 }
 
 dependencies {
@@ -19,4 +40,12 @@ dependencies {
 
 application {
     mainClassName = "RunBenchmarksKt"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    kotlinOptions {
+        freeCompilerArgs += listOf(
+                "-Xuse-experimental=kotlin.ExperimentalUnsignedTypes"
+        )
+    }
 }
